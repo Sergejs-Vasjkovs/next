b4009e1211dc8e33e3372b163845fc2b0366f6ad
Sergejs-Vasjkovs <s.vasjkovs@gmail.com>
2023-12-26 17:49:22 +0200

    db setting up

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Lusitana, Inter } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'], display: 'swap' });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
