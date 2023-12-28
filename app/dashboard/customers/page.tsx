715335f16e57fd7d7dba019cf0fb1b84a20c13da
Sergejs-Vasjkovs <s.vasjkovs@gmail.com>
2023-12-28 20:31:41 +0200

    complete Next JS tutorial

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers Page',
+};
+
 export default function CustomersPage() {
   return <p>Customers Page</p>;
 }
